0cfba35d9b6e4d9a9c1be9880aa68e8f1e301884
Denis <dhoornae@ulb.ac.be>
2018-01-26 23:57:38 +0100

    refactoring

--- src/lexer/Parser.py
+++ src/lexer/Parser.py
@@ -4,6 +4,7 @@ sys.path.append('error/')
 
 from Environment import Environment
 from UnclosedEnvironmentError import UnclosedEnvironmentError
+from PatternNotFoundError import PatternNotFoundError
 
 class Parser():
 
@@ -17,8 +18,8 @@ class Parser():
 
     def readFile(self, filename):
         """
-        Method that loads the content of the file given in parameter into
-        the attribut 'content' of the object Parser.
+        (private) Method that loads the content of the file given in parameter
+        into the attribut 'content' of the object Parser.
         """
         f = open(filename)
         self.content = f.read().strip()
@@ -26,32 +27,62 @@ class Parser():
 
     def isStateLatex(self):
         """
-        Returns whether the previous environment was LaTex or Python.
+        (public) Returns whether the previous environment was LaTex or Python.
         """
         return self.currentEnvironment == Environment.latex
 
+    def indexOf(self, pattern, start=0):
+        """
+        (private) Returns the index of 'pattern' in 'self.content' from a
+        specific start. If the pattern is not found, an error is raised.
+        """
+        index = self.content[start:].find(pattern)
+        if(index >= 0):
+            index += start
+        else: # index == -1
+            raise PatternNotFoundError("The pattern '"+pattern+"' has not been found")
+        return index
+
+    def manageNewEnvironment(self, index, delimiter):
+        """
+        (private) Returns a tuple of the environment and its associated content.
+        In addition, the method manages correctly the object attributs.
+        """
+        res = None
+        if(index >= 0):
+            res = (self.currentEnvironment, self.content[self.cursor:index])
+            self.cursor = index+len(delimiter)
+            self.currentEnvironment = Environment.python if(self.isStateLatex()) else Environment.latex
+        return res
+
+    def manageEndEnvironment(self, delimiter):
+        """
+        (private) Returns a tuple of the environment and its associated content
+        or raises an error if it appears that the python environment has not
+        been closed (i.e. if no '?>' appears in the remaing input).
+        """
+        res = None
+        if(delimiter == "<?"): # and index == -1 : the remaining of the file only contains latex environment
+            res = (Environment.latex, self.content[self.cursor:])
+            self.currentEnvironment = Environment.latex
+            self.cursor = len(self.content)-1
+        else: # delimiter == "?>" and index == -1 : the python environment is not closed
+            raise UnclosedEnvironmentError("The Python environment openned at (x, y) is not closed.")
+        return res
+
     def lex(self):
         """
-        Returns a tuple contaning the detected environment and its content. Will
+        (public) Returns a tuple contaning the detected environment and its content. Will
         eventually raise an error when reaching the end of the file.
         """
+        res = None
         if(self.cursor < len(self.content)-1):
-            res = None
             delimiter = "<?" if(self.isStateLatex()) else "?>"
-            index = self.cursor+self.content[self.cursor:].find(delimiter)
-            print(self.content[self.cursor:], "- index : ", index, delimiter)
-            if(index >= 0):
-                res = (self.currentEnvironment, self.content[self.cursor:index])
-                self.cursor = index+len(delimiter)
-                self.currentEnvironment = Environment.python if(self.isStateLatex()) else Environment.latex
-            elif(delimiter == "<?"): # and index == -1 : the remaining of the file only contains latex environment
-                res = (Environment.latex, self.content[self.cursor:])
-                self.currentEnvironment = Environment.latex
-                self.cursor = len(self.content)-1
-            else: # delimeter == "?>" and index == -1 : the python environment is not closed
-                raise UnclosedEnvironmentError("The Python environment openned at (x, y) is not closed.")
-            print(res)
-            print("--")
-            return res
+            try:
+                index = self.indexOf(delimiter, self.cursor)
+                res = self.manageNewEnvironment(index, delimiter)
+            except PatternNotFoundError as e:
+                res = self.manageEndEnvironment(delimiter)
         else:
             raise EOFError()
+        return res
