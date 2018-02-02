7e4ea7173a71df88db617031f1a97c37b5351c45
Denis <dhoornae@ulb.ac.be>
2018-02-02 12:00:30 +0100

    Correction : wrong file path

--- test/lexer/TestParser.py
+++ test/lexer/TestParser.py
@@ -13,34 +13,34 @@ class TestParser(unittest.TestCase):
         self.parser = None
 
     def test_CorrectParserInitalisation(self):
-        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.ptex")
         self.assertNotEqual(self.parser.content, "")
         self.assertEqual(self.parser.currentEnvironment, Environment.latex)
         self.assertEqual(self.parser.cursor, 0)
 
     def test_isStateLatex(self):
-        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.ptex")
         self.parser.currentEnvironment = Environment.latex
         self.assertTrue(self.parser.isStateLatex())
 
     def test_UncorrectIsStateLatex(self):
-        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.ptex")
         self.parser.currentEnvironment = Environment.python
         self.assertFalse(self.parser.isStateLatex())
 
     def test_NoPythonEnvironment(self):
-        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.ptex")
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, self.parser.content))
 
     def test_NothingElseToRead(self):
-        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/NoPythonEnvironment.ptex")
         res = self.parser.lex()
         with self.assertRaises(EOFError):
             res = self.parser.lex()
 
     def test_OnePythonEnvironment(self):
-        self.parser = Parser("test/lexer/assets/OnePythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/OnePythonEnvironment.ptex")
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
         res = self.parser.lex()
@@ -49,13 +49,13 @@ class TestParser(unittest.TestCase):
         self.assertEqual(res, (Environment.latex, "\n\end{document}"))
 
     def test_UnclosedPythonEnvironment(self):
-        self.parser = Parser("test/lexer/assets/UnclosedPythonEnvironment.tex")
+        self.parser = Parser("test/lexer/assets/UnclosedPythonEnvironment.ptex")
         res = self.parser.lex()
         with self.assertRaises(UnclosedEnvironmentError):
             res = self.parser.lex()
 
     def test_MultiplePythonEnvironments(self):
-        self.parser = Parser("test/lexer/assets/MultiplePythonEnvironments.tex")
+        self.parser = Parser("test/lexer/assets/MultiplePythonEnvironments.ptex")
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
         res = self.parser.lex()
@@ -68,7 +68,7 @@ class TestParser(unittest.TestCase):
         self.assertEqual(res, (Environment.latex, "\n\end{document}"))
 
     def test_TwoConsecutivePythonEnvironments(self):
-        self.parser = Parser("test/lexer/assets/TwoConsecutivePythonEnvironments.tex")
+        self.parser = Parser("test/lexer/assets/TwoConsecutivePythonEnvironments.ptex")
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
         res = self.parser.lex()
