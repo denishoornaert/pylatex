39865e4b762055ee7d8a3ef790da28f8c7ca5753
denishoornaert <dhoornae@ulb.ac.be>
2018-02-02 11:39:27 +0100

    Management of the upcomming errors

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from src.generator import Generator
 
 if (__name__ == '__main__'):
     try:
-        Generator.generates(argv[1])
+        Generator.generates(sys.argv[1])
     except IndexError as e:
         print("Haven't you forgot to specify a '.ptex' file ?")
-    except Exception as e:
+    except EOFError as e:
+        print("Are you sure that you have closed all your python environments ?")
+    else:
         print("Well... Something really unexpected happened.")
-        print(e)
