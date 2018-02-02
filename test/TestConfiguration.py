a627a3ae0ec095f0657766606d7b7a4837de09d6
Denis <dhoornae@ulb.ac.be>
2018-02-02 13:59:25 +0100

    Refactor : get complete path directly from the TestConfiguration class

--- test/TestConfiguration.py
+++ test/TestConfiguration.py
@@ -5,3 +5,9 @@ class TestConfiguration():
     latexTestFilePath = "test/assets/"
 
     pylatexExtension = ".ptex"
+
+    @staticmethod
+    def getCompletePath(filepath):
+        res  = TestConfiguration.latexTestFilePath
+        res += filepath
+        return  res + TestConfiguration.pylatexExtension
--- test/lexer/TestParser.py
+++ test/lexer/TestParser.py
@@ -14,43 +14,40 @@ class TestParser(unittest.TestCase):
     def tearDown(self):
         self.parser = None
 
-    def getCompletePath(self, filename):
-        return config.latexTestFilePath + filename + config.pylatexExtension
-
     def test_CorrectParserInitalisation(self):
-        filepath = self.getCompletePath("NoPythonEnvironment")
+        filepath = config.getCompletePath("NoPythonEnvironment")
         self.parser = Parser(filepath)
         self.assertNotEqual(self.parser.content, "")
         self.assertEqual(self.parser.currentEnvironment, Environment.latex)
         self.assertEqual(self.parser.cursor, 0)
 
     def test_isStateLatex(self):
-        filepath = self.getCompletePath("NoPythonEnvironment")
+        filepath = config.getCompletePath("NoPythonEnvironment")
         self.parser = Parser(filepath)
         self.parser.currentEnvironment = Environment.latex
         self.assertTrue(self.parser.isStateLatex())
 
     def test_UncorrectIsStateLatex(self):
-        filepath = self.getCompletePath("NoPythonEnvironment")
+        filepath = config.getCompletePath("NoPythonEnvironment")
         self.parser = Parser(filepath)
         self.parser.currentEnvironment = Environment.python
         self.assertFalse(self.parser.isStateLatex())
 
     def test_NoPythonEnvironment(self):
-        filepath = self.getCompletePath("NoPythonEnvironment")
+        filepath = config.getCompletePath("NoPythonEnvironment")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, self.parser.content))
 
     def test_NothingElseToRead(self):
-        filepath = self.getCompletePath("NoPythonEnvironment")
+        filepath = config.getCompletePath("NoPythonEnvironment")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         with self.assertRaises(EOFError):
             res = self.parser.lex()
 
     def test_OnePythonEnvironment(self):
-        filepath = self.getCompletePath("OnePythonEnvironment")
+        filepath = config.getCompletePath("OnePythonEnvironment")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
@@ -60,14 +57,14 @@ class TestParser(unittest.TestCase):
         self.assertEqual(res, (Environment.latex, "\n\end{document}"))
 
     def test_UnclosedPythonEnvironment(self):
-        filepath = self.getCompletePath("UnclosedPythonEnvironment")
+        filepath = config.getCompletePath("UnclosedPythonEnvironment")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         with self.assertRaises(UnclosedEnvironmentError):
             res = self.parser.lex()
 
     def test_MultiplePythonEnvironments(self):
-        filepath = self.getCompletePath("MultiplePythonEnvironments")
+        filepath = config.getCompletePath("MultiplePythonEnvironments")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
@@ -81,7 +78,7 @@ class TestParser(unittest.TestCase):
         self.assertEqual(res, (Environment.latex, "\n\end{document}"))
 
     def test_TwoConsecutivePythonEnvironments(self):
-        filepath = self.getCompletePath("TwoConsecutivePythonEnvironments")
+        filepath = config.getCompletePath("TwoConsecutivePythonEnvironments")
         self.parser = Parser(filepath)
         res = self.parser.lex()
         self.assertEqual(res, (Environment.latex, "\documentclass[a4paper,11pt]{article}\n\\begin{document}\n\section{test}\n"))
