b3771257a8395364f223630a081550cdab6ad9d5
denishoornaert <dhoornae@ulb.ac.be>
2018-02-02 11:30:06 +0100

    Management of the python environments : sent to interpretation

--- src/generator/Generator.py
+++ src/generator/Generator.py
@@ -1,6 +1,8 @@
 import os
 from src.fileController import FileController
 from src.lexer import Parser
+from src.lexer.struct import Environment
+from src.interpreter import Interpreter
 
 class Generator():
 
@@ -9,18 +11,15 @@ class Generator():
     @staticmethod
     def generates(pyLaTeXFile):
         # Set data
-        parser  = Parser(pyLaTeXFile)
+        parser = Parser(pyLaTeXFile)
         data = ""
         # Main part of the body
-        try:
-            while(True):
-                res = parser.lex()
+        while(True):
+            res = parser.lex()
+            if(res[0] == Environment.python):
+                data += Interpreter.execute(res[1])
+            else:
                 data += res[1]
-        except EOFError as e:
-            # Do nothing as the end of the file as been reached
-            pass
-        else:
-            raise Exception
         # End of the process : write the generated LaTeX file
         destFile = os.path.splitext(pyLaTeXFile)[0]+".tex"
-        FileController.write(data, destFile)
+        FileController.write(destFile, data)
