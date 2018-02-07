891204f97587e74ddb6aace767cd3a5fe8ff405f
Denis <dhoornae@ulb.ac.be>
2018-02-07 20:05:43 +0100

    Addition of features : header + column

--- test/utils/latex/TestTabular.py
+++ test/utils/latex/TestTabular.py
@@ -43,3 +43,52 @@ class TestTabular(unittest.TestCase):
         res = tabular.__latex__()
         answer = "\\begin{tabular}{|c|}\n\t\\hline\n\t 1 \\\\ \n\t\\hline\n\t [1, 1] \\\\ \n\t\\hline\n\t [1, 2, 1] \\\\ \n\t\\hline\n\\end{tabular}\n"
         self.assertEqual(res, answer)
+
+    def test_tabularWithHeader(self):
+        matrix = [[i*j for j in range(3)] for i in range(3)]
+        tabular = Tabular(matrix)
+        tabular.setHeader(["a", "b", "c"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|}\n\t\\hline\n\t a & b & c \\\\ \n\t\\hline\n\t 0 & 0 & 0 \\\\ \n\t\\hline\n\t 0 & 1 & 2 \\\\ \n\t\\hline\n\t 0 & 2 & 4 \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
+
+    def test_tabularWithHeaderLargerThanMatrix(self):
+        matrix = [[i*j for j in range(2)] for i in range(3)]
+        tabular = Tabular(matrix)
+        tabular.setHeader(["a", "b", "c"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|}\n\t\\hline\n\t a & b & c \\\\ \n\t\\hline\n\t 0 & 0 &   \\\\ \n\t\\hline\n\t 0 & 1 &   \\\\ \n\t\\hline\n\t 0 & 2 &   \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
+
+    def test_tabularWithHeaderSmallerThanMatrix(self):
+        matrix = [[i*j for j in range(3)] for i in range(2)]
+        tabular = Tabular(matrix)
+        tabular.setHeader(["a", "b"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|}\n\t\\hline\n\t a & b &   \\\\ \n\t\\hline\n\t 0 & 0 & 0 \\\\ \n\t\\hline\n\t 0 & 1 & 2 \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
+
+    def test_tabularWithSideColumn(self):
+        matrix = [[i*j for j in range(2)] for i in range(2)]
+        tabular = Tabular(matrix)
+        tabular.setSideColumn(["a", "b"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|}\n\t\\hline\n\t a & 0 & 0 \\\\ \n\t\\hline\n\t b & 0 & 1 \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
+
+    def test_tabularWithSideColumnLargerThanMatrix(self):
+        matrix = [[i*j for j in range(2)] for i in range(2)]
+        tabular = Tabular(matrix)
+        tabular.setSideColumn(["a", "b", "c"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|}\n\t\\hline\n\t a & 0 & 0 \\\\ \n\t\\hline\n\t b & 0 & 1 \\\\ \n\t\\hline\n\t c &   &   \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
+
+    def test_tabularWithHeaderAndSideColumn(self):
+        matrix = [[i*j for j in range(3)] for i in range(3)]
+        tabular = Tabular(matrix)
+        tabular.setHeader(["a", "b", "c"])
+        tabular.setSideColumn(["a", "b", "c"])
+        res = tabular.__latex__()
+        answer = "\\begin{tabular}{|c|c|c|c|}\n\t\\hline\n\t   & a & b & c \\\\ \n\t\\hline\n\t a & 0 & 0 & 0 \\\\ \n\t\\hline\n\t b & 0 & 1 & 2 \\\\ \n\t\\hline\n\t c & 0 & 2 & 4 \\\\ \n\t\\hline\n\\end{tabular}\n"
+        self.assertEqual(res, answer)
--- utils/latex/Tabular.py
+++ utils/latex/Tabular.py
@@ -6,6 +6,8 @@ class Tabular():
         if(type(array) == list):
             self.array = array
             self.oneliner = False
+            self.header = None
+            self.sideColumn = None
             self.rowSize = self.getMaxRowSize()
             self.columnSize = len(self.array)
         else:
@@ -13,6 +15,14 @@ class Tabular():
 
     def __str__(self):
         return self.array.__str__()
+
+    def setHeader(self, header):
+        self.header = header
+        self.rowSize = max(self.rowSize, len(self.header))
+
+    def setSideColumn(self, column):
+        self.sideColumn = column
+        self.columnSize = max(self.columnSize, len(self.sideColumn))
 
     def getMaxRowSize(self):
         """
@@ -24,24 +34,48 @@ class Tabular():
         self.oneliner = not all([type(elem) == list for elem in self.array])
         if(not self.oneliner):
             rowsSize = []
+            if(self.header != None):
+                rowsSize.append(len(self.header))
             for row in self.array:
                 rowsSize.append(len(row))
             res = max(rowsSize)
         return res
 
     def generateHeader(self):
-        return "\\begin{tabular}{|"+("c|"*self.rowSize)+"}\n\t\\hline\n\t "
+        """
+        (private) Genrates the first part of the table.
+        """
+        nbColmun = self.rowSize+(self.sideColumn != None)
+        return "\\begin{tabular}{|"+("c|"*nbColmun)+"}\n\t\\hline\n\t "
 
     def generateFooter(self):
+        """
+        (private) Genrates the last (and third part) of the table.
+        """
         return "\\end{tabular}\n"
 
     def generateBody(self):
+        """
+        (private) Genrates the middle part of the table.
+        """
         res = ""
+        if(self.header != None):
+            if(self.sideColumn != None):
+                res += " "+" & "
+            for col in range(self.rowSize):
+                try:
+                    res += str(self.header[col])
+                except IndexError as e:
+                    res += " "
+                res += " \\\\ \n" if(col==self.rowSize-1) else " & "
+            res += "\t\\hline\n\t "
         for row in range(self.columnSize):
             if(self.oneliner):
                 res += str(self.array[row])
                 res += " \\\\ \n"
             else:
+                if(self.sideColumn != None):
+                    res += self.sideColumn[row]+" & "
                 for col in range(self.rowSize):
                     try:
                         res += str(self.array[row][col])
@@ -49,7 +83,7 @@ class Tabular():
                         res += " "
                     res += " \\\\ \n" if(col==self.rowSize-1) else " & "
             res += "\t\\hline\n"
-            res += "" if(row==len(self.array)-1) else "\t "
+            res += "" if(row==self.columnSize-1) else "\t "
         return res
 
     def __latex__(self):
